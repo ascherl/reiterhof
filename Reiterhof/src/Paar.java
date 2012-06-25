ba3bbfc18a42a99b8ba3bb4474217685503686e1
jak2011math <mai11fri@studserv.uni-leipzig.de>
2012-06-25 22:30:57 +0200

    kj

---
+++ Reiterhof/src/Paar.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Paar {
+
+	
+	private Pferd  Pferd;
+	private  Reiter Reiter;
+	
+	private ArrayList<Paar> Paar;
+
+	public ArrayList<Paar> getPaar() {
+		return Paar;
+	}
+
+	public void setPaar(ArrayList<Paar> paar) {
+		Paar = paar;
+	}
+	
+}
