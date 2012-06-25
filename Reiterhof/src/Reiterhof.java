49a10a3217fb16f6afda5a0b06b426f5d96935db
Rudi Ascherl <info@rudiascherl.de>
2012-06-25 22:16:42 +0200

    KACKE

--- Reiterhof/src/Reiterhof.java
+++ Reiterhof/src/Reiterhof.java
@@ -2,11 +2,23 @@ import java.util.ArrayList;
 
 
 public class Reiterhof {
+	private ArrayList<Reiter> ReiterListe;
+	private ArrayList<Pferd> PferdeListe;
+
 	Reiterhof(ArrayList<Reiter> ReiterListe, ArrayList<Pferd> Pferde) {
-		
+		ArrayList<Reiter> this.ReiterListe = ReiterListe;
+		ArrayList<Pferd> this.PferdeListe = Pferde;
 	}
 	
 	public ArrayList<Reiter> zuordnen() {
+		
+		for(int i = 1; i <= ReiterListe.size(); i++)
+		{
+			if(ReiterListe.get(i).getWuensche().size() == 1)
+			{
+				setPaar();
+			}
+		}
 		return ;
 	}
 }
